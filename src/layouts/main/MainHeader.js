785e7bcdd2aaac82bea90194b4341dcc57bb7d91
itssabhishek <itsabhishek0710@gmail.com>
2023-03-09 22:15:02 +0530

    Created PWA

--- src/layouts/main/MainHeader.js
+++ src/layouts/main/MainHeader.js
@@ -65,7 +65,7 @@ export default function MainHeader() {
           <Logo />
 
           <Label color="info" sx={{ ml: 1 }}>
-            v1.0.0
+            v1.2.1
           </Label>
           <Box sx={{ flexGrow: 1 }} />
         </Container>
